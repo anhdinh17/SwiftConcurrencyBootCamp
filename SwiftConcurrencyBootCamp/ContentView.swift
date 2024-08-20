8b3cddf47d786694376d0feedad07a69ea73cc3b
Anh Dinh <anhdinh17@gmail.com>
2024-08-20 17:48:14 +0700

    Initial Commit

---
+++ SwiftConcurrencyBootCamp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftConcurrencyBootCamp
+//
+//  Created by Anh Dinh on 8/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftConcurrencyBootCamp/SwiftConcurrencyBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyBootCampApp.swift
+//  SwiftConcurrencyBootCamp
+//
+//  Created by Anh Dinh on 8/20/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
